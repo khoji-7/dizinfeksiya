a954e9d1474cb12987267da79480825b7959a06b
khoji-7 <nematxonovh@gmail.com>
2024-07-15 21:26:30 +0500

    Update boldi

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import "./assets/main.css"
-
-import { createApp } from 'vue'
+import { createApp } from "vue"
 import App from './App.vue'
 import EN from "./i18n/en.json"
 import UZ from "./i18n/uz.json"
-createApp(App).mount('#app')
+import { createI18n } from "vue-i18n"
 
-import { createI18n, useI18n } from "vue-i18n"
-
+// I18n ni yaratish va sozlash
 const i18n = createI18n({
-    locale:'EN',
-    messages:{
-        EN:EN,
-        UZ:UZ
-    }    
+    locale: 'EN',
+    messages: {
+        EN: EN,
+        UZ: UZ
+    }
 })
 
+// Vue ilovasini yaratish va i18n ni ulash
+const app = createApp(App)
+app.use(i18n)
+app.mount('#app')
